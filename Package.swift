8b17814945c52d2efd5a2b3eb0ad1550022e196e
Adin Cebic <cebic.ad@gmail.com>
2024-03-04 10:55:44 +0100

    Reference locally stored dynamic BlinkReceipt framework and Rename it from BlinkReceiptStatic to just BlinkReceipt

--- Package.swift
+++ Package.swift
@@ -3,21 +3,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "BlinkReceiptStatic",
+    name: "BlinkReceipt",
     platforms: [
         .iOS(.v12)
     ],
     products: [
         .library(
-            name: "BlinkReceiptStatic",
-            targets: ["BlinkReceiptStatic"]),
+            name: "BlinkReceipt",
+            targets: ["BlinkReceipt"]),
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "BlinkReceiptStatic",
-            url: "https://github.com/BlinkReceipt/blinkreceipt-ios/releases/download/1.45.0/BlinkReceiptStatic.xcframework-1.45.0.zip",
-            checksum: "7649f09ba9641c816b5250dd6bb1d1493ff085448b0c547d8d913526d20a832e"
+            name: "BlinkReceipt",
+            path: "BlinkReceipt.xcframework"
         )
     ]
 )

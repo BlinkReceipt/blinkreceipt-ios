881160cd99c4485bcafcbf94ad9d7f454ed2cc49
Adin Cebic <cebic.ad@gmail.com>
2024-06-17 11:25:01 +0200

    Temporarily add BlinkReceiptStatic target to the Package.swift manifest

--- Package.swift
+++ Package.swift
@@ -12,12 +12,20 @@ let package = Package(
         .library(
             name: frameworkName,
             targets: [frameworkName]),
+        .library(
+            name: "\(frameworkName)Static",
+            targets: ["\(frameworkName)Static"]),
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
             name: frameworkName,
             path: "\(frameworkName).xcframework"
+        ),
+        .binaryTarget(
+            name: "\(frameworkName)Static",
+            url: "https://github.com/BlinkReceipt/blinkreceipt-ios/releases/download/1.49.0/BlinkReceiptStatic.xcframework-1.49.0.zip",
+            checksum: "775b166501c67d219dc47d93dc2edcf62303707ebd44289f38eed3ffd4a6b74e"
         )
     ]
 )

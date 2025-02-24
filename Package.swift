afc5476ee999b5c08ff33e4f43b7f22edfc2ec82
Stanislav <stanislav.ivanov@microblink.com>
2025-02-24 15:36:03 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "\(frameworkName)Static",
-            url: "https://github.com/BlinkReceipt/blinkreceipt-ios/releases/download/1.55.0/BlinkReceiptStatic.xcframework-1.55.0.zip,",
+            url: "https://github.com/BlinkReceipt/blinkreceipt-ios/releases/download/1.55.0/BlinkReceiptStatic.xcframework-1.55.0.zip",
             checksum: "17b811cfa70e92b3628f19ed1f21baeecc103d041b3f791253d8634edfbc59b7"
         )
     ]

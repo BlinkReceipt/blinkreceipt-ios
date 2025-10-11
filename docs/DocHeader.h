4f8c8be2c74a0ceb4b12e34f0deaf46fc37099ea
Danny Panzer <danny.panzer@microblink.com>
2025-10-11 16:59:26 -0400

    update docs

---
+++ docs/DocHeader.h
@@ -0,0 +1,24 @@
+//
+//  DocHeader.h
+//  BlinkReceipt
+//
+//  Created by Danny Panzer on 1/5/19.
+//  Copyright © 2019 Windfall. All rights reserved.
+//
+
+#ifndef DocHeader_h
+#define DocHeader_h
+
+#import "BRProduct.h"
+#import "BRCoupon.h"
+#import "BRShipment.h"
+#import "BRScanManager.h"
+#import "BRScanOptions.h"
+#import "BRScanResults.h"
+#import "BRScanResultsDelegate.h"
+#import "BRCameraViewController.h"
+#import "BRMissedEarningsBaseViewController.h"
+#import "BRSurvey.h"
+#import "BRPromotion.h"
+
+#endif /* DocHeader_h */
